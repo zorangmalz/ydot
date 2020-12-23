0a69b22ef1738e850ad07caf6b86af4d55e1ca47
jungsunwoung <51280827+jungsunwoung@users.noreply.github.com>
2020-12-23 14:38:13 +0900

    caver-js-ext-kas

--- frontend/ydot/src/components/Home/HomeMain.js
+++ frontend/ydot/src/components/Home/HomeMain.js
@@ -19,18 +19,30 @@ import Creatortwo from '../icon/Creatortwo.png'
 import Creatorthree from '../icon/Creatorthree.png'
 import Creatorfour from '../icon/Creatorfour.png'
 
+import CaverExtKAS from "caver-js-ext-kas"
+
 
 
 export default function HomeMain() {
+    
     const history = useHistory()
     const firebase = useFirebase()
     const firestore = useFirestore()
     const { uid } = useSelector((state) => state.firebase.auth);
     const [items, setItems] = useState([]);
-
+    const chainId=1001
+    const accessKeyId="KASK8QUCLZUJ1K1YZ9GB2VJ2"
+    const secretAccessKey="BkbIcfQfJuD9IrEZawH3+0ML7uARiyw910cEHiOH"
+   
+    async function kasTest(){
+        const caver = new CaverExtKAS()
+        caver.initKASAPI(chainId, accessKeyId, secretAccessKey)
+        const blockNumber = await caver.rpc.klay.getBlockNumber()
+console.log(blockNumber)
+    }
     useEffect(() => {
         load()
-        
+        kasTest()
     }, [])
     // function add(){
     //     firestore.collection("User").doc(uid).set({
