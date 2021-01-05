71c99d478564e1d679defd426b9c109170013100
jungsunwoung <51280827+jungsunwoung@users.noreply.github.com>
2021-01-05 14:56:05 +0900

    Revert "creator"
    This reverts commit a9285878e00cd25904cf6686bfc398b74af363a5.

--- frontend/ydot/src/components/Home/Creator.js
+++ frontend/ydot/src/components/Home/Creator.js
@@ -983,9 +983,15 @@ export default function Creator() {
                                             width: 290,
                                             height: 156,
                                         }}>
-                                           
+                                            <div style={{
+                                                opacity: 0.6,
+                                                color: "#202426",
+                                                fontSize: 18,
+                                                height: 26,
+                                                marginBottom: 10
+                                            }}>인기 동영상</div>
                                             <a href ={popular} target="_blank">
-                                            <img src={popularImg} style={{ width: 290, height: 156}} />
+                                            <img src={popularImg} style={{ width: 290, height: 108 }} />
                                             </a>
                                         </div>
                                     </div>
@@ -1782,7 +1788,12 @@ export default function Creator() {
                                                 maxWidth: 375,
                                                 height: 120,
                                             }}>
-                                                
+                                                <div style={{
+                                                    opacity: 0.6,
+                                                    color: "#202426",
+                                                    fontSize: 14,
+                                                    marginBottom: 5
+                                                }}>인기 동영상</div>
                                                 <a href={popular} target="_blank">
                                                 <img src={popularImg} style={{ objectFit: "cover", width: "85vw" }} />
                                                 </a>
